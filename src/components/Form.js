f2053ff58252ae82b2ff6398d42b28964ceef26b
mucarb <murilo.rb60@gmail.com>
2022-09-15 21:43:35 -0300

    useState in practice

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 function Form() {
+
+  // declarando um useState [<nome do atributo>, <função para alterar>] = <chamando usState()>
+  const [name, setName] = useState();// useState(<(opcional) valor padrão>)
+  const [password, setPassword] = useState();
 
   function register(e) {
     e.preventDefault(); // previnindo o recarregamento da página ao enviar formulario (e -> evento da DOM)
-    console.log('Novo registro executado!');
+    console.log(`Usuário ${name} cadastrado, a senha é ${password}`);
   }
 
   return (
@@ -11,7 +17,13 @@ function Form() {
       {/* criando evento para envio de formulário */}
       <form onSubmit={register}>
         <div>
-          <input type="text" placeholder="Digite alguma coisa!" />
+          <label htmlFor="name">Nome:</label>
+          {/* chamando evento de mudança da variavel para receber o valor, alterando seu state */}
+          <input type="text" id="name" name="name" placeholder="Informe um nome!" onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input type="password" id="password" name="password" placeholder="Digite uma senha!" onChange={(e) => setPassword(e.target.value)} />
         </div>
         <div>
           <input type="submit" value="Cadastrar" />
