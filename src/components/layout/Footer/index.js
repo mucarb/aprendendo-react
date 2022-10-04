46b7092a140f09e91e4688fa7302c077036e2a0e
mucarb <murilo.rb60@gmail.com>
2022-10-04 18:04:00 -0300

    React Icons

--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -1,6 +1,19 @@
+// instalando a bibliotecas de icones
+// npm install react-icons
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
+import './styles.css'
+
 function Footer() {
   return (
-    <footer>Rodapé</footer>
+    <footer>
+      <ul className='social-list'>
+        <li><FaFacebook /></li>
+        <li><FaInstagram /></li>
+        <li><FaLinkedin /></li>
+      </ul>
+      <p>Rodapé</p>
+    </footer>
   );
 }
 
